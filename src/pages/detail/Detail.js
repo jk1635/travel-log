d3737e7de00374bef05f7c849de5d48bf3fa1214
greencoffee1635 <166354@naver.com>
2022-07-17 15:52:57 +0900

    feat: API 연동

--- src/pages/detail/Detail.js
+++ src/pages/detail/Detail.js
@@ -1,9 +1,59 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import api from "../../common/utils/API";
+
+// components
+import { Error, Loading } from "../../common/components";
 
 const Detail = () => {
-  return (
-    <div>Detail</div>
-  );
+  const apiKey = process.env.REACT_APP_SERVICE_API_KEY;
+  const [poi, setPoi] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const lng = 126.985542;
+  const lat = 37.570229;
+
+  useEffect(() => {
+    const getLocationData = async () => {
+      try {
+        setError(null);
+        setLoading(true);
+        const response = await api.get(
+          `/locationBasedList?serviceKey=${apiKey}`,
+          {
+            params: {
+              apiKey,
+              numOfRows: 20,
+              pageNo: 1,
+              MobileOS: "ETC",
+              MobileApp: "init",
+              contentTypeId: 15,
+              arrange: "A",
+              mapX: lng,
+              mapY: lat,
+              radius: 3000,
+              listYN: "Y",
+            },
+          }
+        );
+        console.log("data", response.data);
+        setPoi(response.data);
+      } catch (error) {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+        setError(error);
+      }
+      setLoading(false);
+    };
+    getLocationData();
+  }, []);
+
+  if (loading) return <Loading />;
+  if (error) return <Error />;
+  if (!poi) return null;
+  return <div>Detail</div>;
 };
 
 export default Detail;
